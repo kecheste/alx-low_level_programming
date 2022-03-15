b907ab8e6735e3f74154d4d61f4b22de0633b59b
kecheste <aben.ezer443@gmail.com>
2022-03-15 14:24:26 -0700

    lite

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * main function doesn't return any value
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
  */
-int main(void)/*The main func  */
+int main(void)
 {
-	printf("_putchar\n");
+	char putchar[] = "_putchar";
+	int i = 0;
+
+	while (putchar[i] != '\0')
+	{
+		_putchar(putchar[i]);
+		i++;
+	}
+	_putchar('\n');
+
 	return (0);
 }
