d4f9a0e47a666cb20c87d04ef66ccc8660e103eb
kecheste <aben.ezer443@gmail.com>
2022-03-17 15:55:11 -0700

    mitty

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int _isupper(int c);
+
+int main(){
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
+
+int _isupper(int c){
+
+    if(c > 64 && c < 90){
+        return (1);
+    }else{
+        return (0);
+    }
+}
