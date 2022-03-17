a84708a3e4fd86e2b5191694a85d4919624ebf1a
kecheste <aben.ezer443@gmail.com>
2022-03-17 16:13:16 -0700

    gyt

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,21 +7,23 @@
  * Return: Always 0.
  */
 
-int main(void){
-    char c;
+int main(void)
+{
+	char c;
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
-
-int _isupper(int c){
-
-    if(c > 64 && c < 90){
-        return (1);
-    }else{
-        return (0);
-    }
+int _isupper(int c)
+{
+	if (c > 64 && c < 90)
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
 }
